73081bf67c273c034d34d75aac8ebd3b6d9324c1
Tiaaaaaa <43710032+Tiaaaaaa@users.noreply.github.com>
2019-10-07 19:04:14 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,29 +1,59 @@
-const Serialport = require("serialport");
-const Readline = require("@serialport/parser-readline")
-const store = require('../client/src/store')
+const SerialPort = require("serialport");
+const Readline = require("@serialport/parser-readline");
 
-/*
+let port;
+let parser;
+
 SerialPort.list((err, list) => {
   for (i in list) {
-    if (list[i].manufacturer === "Arduino (www.arduino.cc)") {
-      const port = new SerialPort(list[i].comName, { baudRate: 9600 });
-      const parser = port.pipe(new Readline({ delimiter: "\n" }));
+  //  if (list[i].manufacturer === "Arduino (www.arduino.cc)")
+    if (list[i].ID === "2341:0042") {
+
+      port = new SerialPort(list[i].comName/*"/dev/ttyACM0"*/, { baudRate: 9600 });
+      parser = port.pipe(new Readline({ delimiter: "\n" }));
 
       // Read the port data
       port.on("open", () =>
         console.log(`port open. Data rate: ${port.baudRate}`)
       );
+
+      let i = 0;
+      let array = [];
+
       parser.on("data", data => {
-        console.log("got word from arduino:", data);
+
+        array[i] = data;
+        console.log(array[i]);
+
       });
-      setTimeout(() => {
-        port.write("Hello world", err => {
-          if (err) return console.log("Error on write: ", err.message);
-          console.log("message written");
-        });
-      }, 500);
+
+      i++;
+
+      if(i === 5) i = 0;
+
+
+
+      let input = "b"; //VARIABILE DA COLLEGARE
+
+      if(input != "0"){
+
+        setTimeout(() => {
+          port.write(input, err => {
+
+            if (err) return console.log("Error on write: ", err.message);
+            console.log("message written");
+            input = "0";
+
+          });
+        }, 500);
+      }else{
+      }
+
+
     } else {
     }
+
+    console.log("No arduino attached found");
+
   }
 });
-*/
